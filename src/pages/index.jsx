a9284139bb94aa2b17765d40cdbeb98191e0cd77
Stan <Wiekstras@gmail.com>
2023-02-16 11:37:16 +0100

    Remove twitter

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -275,11 +275,6 @@ export default function Home({ articles }) {
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com/stan_btw"
-              aria-label="Follow on Twitter"
-              icon={TwitterIcon}
-            />
-            <SocialLink
               href="https://github.com/wiekstras"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
